f7197f3dc38d6f19c561512fa93386abec2a7ae7
dan-aksenov <dan-aksenov@yandex.ru>
2016-06-14 11:05:42 +0300

    backup saa
    fixes

--- swift/backup_saa.cmd
+++ swift/backup_saa.cmd
@@ -1,7 +1,6 @@
-IF NOT EXIST log MD log
-set log=%date:~-10,2%%date:~-7,2%%date:~-4,4%.log
+set log=%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%.log
 
-set tag=day
+set tag=min
 set src=192.168.12.1
 
 set SRC_MIRROR=\\%SRC%\m$\SAA_DBRECOVERY_sbxaruee
@@ -14,14 +13,14 @@ IF NOT EXIST %DST_MIRROR% MD %DST_MIRROR%
 IF NOT EXIST %DST_BACKUP% MD %DST_BACKUP%
 
 REM MOVE "current" to "previous"
-del %DST_MIRROR%\prev\prev.rar /y
+del %DST_MIRROR%\prev\prev.rar
 rar a %DST_MIRROR%\prev\prev.rar -m5 -df -r %DST_MIRROR%\curr
 
 rem backup "source" to "current"
 robocopy %SRC_MIRROR% %DST_MIRROR%\curr /LOG+:"log\%log%" /NJH /NFL /NP /NDL /MIR /MT /z 
 
 rem copy "current" to "previous"
-del %DST_BACKUP%\prev\prev.rar /y
+del %DST_BACKUP%\prev\prev.rar
 rar a %DST_BACKUP%\prev\prev.rar -m5 -df -r %DST_BACKUP%\curr
 
 rem backup "source" to "current"
