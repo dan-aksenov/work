9471187f204ac7f0353798a33b21a6b5452a05ee
dan-aksenov <dan-aksenov@yandex.ru>
2016-06-15 18:16:10 +0300

    jiraissue rollback date
    more fixes

--- jira_hack/rollback_date.sql
+++ jira_hack/rollback_date.sql
@@ -3,6 +3,8 @@ DECLARE
    l_issue_id   NUMBER := 100104;
 BEGIN
    --UPDATE JIRAISSUE
+   DBMS_OUTPUT.put_line ('JIRAISSUE');
+
    UPDATE JIRAISSUE
       SET (created, updated) =
              (SELECT created - l_days_ago created,
@@ -14,6 +16,8 @@ BEGIN
    DBMS_OUTPUT.put_line (SQL%ROWCOUNT);
 
    --UPDATE JIRAACTION
+   DBMS_OUTPUT.put_line ('JIRAACTION');
+
    FOR i IN (SELECT id
                FROM jiraaction
               WHERE issueid = l_issue_id)
@@ -29,19 +33,39 @@ BEGIN
       DBMS_OUTPUT.put_line (SQL%ROWCOUNT);
    END LOOP;
 
-FOR i IN (SELECT ID
+   --update CHANGEGROUP
+   DBMS_OUTPUT.put_line ('CHANGEGROUP');
+
+   FOR i IN (SELECT ID
                FROM CHANGEGROUP
               WHERE issueid = l_issue_id)
    LOOP
       UPDATE CHANGEGROUP
          SET (created) =
                 (SELECT created - l_days_ago created
-                        FROM JIRAACTION
+                   FROM JIRAACTION
                   WHERE id = i.id)
        WHERE id = i.id;
+
       DBMS_OUTPUT.put_line (SQL%ROWCOUNT);
    END LOOP;
 
-   COMMIT;
+   DBMS_OUTPUT.put_line ('WORKLOG');
+
+   FOR i IN (SELECT ID
+               FROM WORKLOG
+              WHERE issueid = l_issue_id)
+   LOOP
+      UPDATE WORKLOG
+         SET (created) =
+                (SELECT created - l_days_ago created
+                   FROM WORKLOG
+                  WHERE id = i.id)
+       WHERE id = i.id;
+
+      DBMS_OUTPUT.put_line (SQL%ROWCOUNT);
+   END LOOP;
+
+   ROLLBACK;
 END;
 /
