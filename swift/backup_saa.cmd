7bdd23fae48c21bc3ad24013f1bd40618fbad815
dan-aksenov <dan-aksenov@yandex.ru>
2016-06-16 10:11:39 +0300

    backup_saa
    Explanation comments added.

--- swift/backup_saa.cmd
+++ swift/backup_saa.cmd
@@ -1,6 +1,10 @@
+REM RUN VARIABLE SECTION
+rem set log=%date:~-10,2%%date:~-7,2%%date:~-4,4%.log
 set log=%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%.log
 
+rem set tag=day
 set tag=min
+
 set src=192.168.12.1
 
 set SRC_MIRROR=\\%SRC%\m$\SAA_DBRECOVERY_sbxaruee
@@ -12,22 +16,24 @@ set DST_BACKUP=E:\BACKUP\%tag%
 IF NOT EXIST %DST_MIRROR% MD %DST_MIRROR%
 IF NOT EXIST %DST_BACKUP% MD %DST_BACKUP%
 
-REM MOVE "current" to "previous"
+REM END RUN VARIABLE SECTION
+
+REM MOVE "current" MIRROR to "previous"
 del %DST_MIRROR%\prev\prev.rar
 rar a %DST_MIRROR%\prev\prev.rar -m5 -df -r %DST_MIRROR%\curr
 
-rem backup "source" to "current"
+rem backup "source" MIRROR to "current"
 robocopy %SRC_MIRROR% %DST_MIRROR%\curr /LOG+:"log\%log%" /NJH /NFL /NP /NDL /MIR /MT /z 
 
-rem copy "current" to "previous"
+rem copy "current" BACKUP to "previous"
 del %DST_BACKUP%\prev\prev.rar
 rar a %DST_BACKUP%\prev\prev.rar -m5 -df -r %DST_BACKUP%\curr
 
-rem backup "source" to "current"
+rem backup "source" BACKUP to "current"
 robocopy %SRC_BACKUP% %DST_BACKUP%\curr /LOG+:"log\%log%" /NJH /NFL /NP /NDL /MIR /MT /z 
 
 rem summary
-findstr /r "Dirs Files Bytes Ended" log\%log% | find /v "*.*" >> log\summary_%tag%.log
+findstr /r "Dirs Files Bytes Ended" "log\%log%" | find /v "*.*" >> log\summary_%tag%.log
 echo ############################################################################## >> log\summary_%tag%.log
 
 rem purge older logs
