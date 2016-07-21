72f5b7bc25e156a3305ca445ee1cba095e32614c
dan-aksenov <dan-aksenov@yandex.ru>
2016-07-21 13:10:49 +0300

    swift_push_backup
    fixes

--- swift/push_backup.cmd
+++ swift/push_backup.cmd
@@ -1,3 +1,5 @@
+REM %1 - backup tag (day/min), %2 remote site, %3 - remode drive
+
 REM General setup
 IF NOT EXIST log MD log
 set TAG=%1
@@ -5,7 +7,7 @@ IF %TAG%==day set STAMP=%date:~-10,2%%date:~-7,2%%date:~-4,4%
 IF %TAG%==min set STAMP=%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%
 
 REM Remote site 
-set DEST="\\%2\c$\REMOTE_BACKUP\%tag%_%STAMP%"
+set DEST="\\%2\%3%$\REMOTE_BACKUP\%TAG%_%STAMP%"
 IF NOT EXIST %DEST% MD %DEST%
 IF NOT EXIST %DEST%\MIRROR MD %DEST%\MIRROR
 IF NOT EXIST %DEST%\BACKUP MD %DEST%\BACKUP
@@ -15,6 +17,10 @@ SET SRC_MIRROR=m:\SAA_DBRECOVERY_sbxaruee
 SET SRC_BACKUP=f:\SAA_DBRECOVERY_sbxaruee
 SET STAGE_MIRROR=m:\MIRROR
 SET STAGE_BACKUP=f:\BACKUP
+
+REM clear staging area
+IF EXIST %STAGE_MIRROR% rd %STAGE_MIRROR% /q /s
+IF EXIST %STAGE_BACKUP% rd %STAGE_BACKUP% /q /s
 IF NOT EXIST %STAGE_MIRROR% MD %STAGE_MIRROR%
 IF NOT EXIST %STAGE_BACKUP% MD %STAGE_BACKUP%
 
@@ -23,16 +29,11 @@ xcopy %SRC_MIRROR% %STAGE_MIRROR% /s
 xcopy %SRC_BACKUP% %STAGE_BACKUP% /s
 
 REM Copy staging to remote site
-robocopy %STAGE_MIRROR% %DEST%\MIRROR /LOG+:"log\%STAMP%.log" /NJH /NFL /NP /NDL /MIR /MT /z 
-robocopy %STAGE_BACKUP% %DEST%\BACKUP /LOG+:"log\%STAMP%.log" /NJH /NFL /NP /NDL /MIR /MT /z 
-
+robocopy %STAGE_MIRROR% %DEST%\MIRROR /LOG+:"log\%STAMP%.log" /MIR /NJH /NFL /NP /NDL /MIR /MT /z 
+robocopy %STAGE_BACKUP% %DEST%\BACKUP /LOG+:"log\%STAMP%.log" /MIR /NJH /NFL /NP /NDL /MIR /MT /z 
 
 rem backup summary
 findstr /r "Dirs Files Bytes Ended Times" "log\%log%" | find /v "*.*" >> log\summary_%TAG%.log
 echo ############################################################################## >> log\summary_%TAG%.log
-
-REM clear staging area
-rd %STAGE_MIRROR%
-rd %STAGE_MIRROR%
 
 exit
