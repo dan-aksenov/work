1be8e7c669d936bae39304880ddd164007e4b351
dan-aksenov <dan-aksenov@yandex.ru>
2017-08-30 11:54:52 +0300

    db patch scripts redone

---
+++ skpdi/db_patch_skpdi.bat
@@ -0,0 +1,36 @@
+﻿@chcp 65001
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+REM UPDATE Skpdi server.
+REM Set database's host from first parameter.
+SET PGHOST=gudhskpdi-db-01
+
+set PGPORT=5432
+set PGUSER=ods
+set PGPASSWORD=ods
+
+REM Set database name from second parameter
+set PGDATABASE=ods_prod
+
+set script_name=install_db
+set width=120
+set height=60
+mode con:cols=%width% lines=%height%
+
+set "bat_name=%~n0"
+set "bat_path=%~dp0"
+set "run_path=%bat_path%"
+
+cd %run_path%
+
+set "spool_file=%run_path%%script_name%_log.log"
+set "run_file=%run_path%%script_name%.sql"
+
+rem Запуск
+psql <%run_file% >%spool_file% 2>&1
+
+psql -c "DELETE FROM core.fdc_sys_class_impl_lnk;"
+psql -c "DELETE FROM core.fdc_sys_class_impl;"
+psql -c "DELETE FROM core.fdc_sys_class_panel_lnk;"
+psql -c "DELETE FROM core.fdc_sys_class_panel;"
+
+exit
