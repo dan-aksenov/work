1e0eb6307164b1ad86ee306c03eff0a68ee9da65
dan-aksenov <dan-aksenov@yandex.ru>
2017-09-19 11:13:39 +0300

    recreate stage dir.

--- skpdi/1_get_from_sunny.bat
+++ skpdi/1_get_from_sunny.bat
@@ -1,4 +1,6 @@
 REM create sunny path. %1 is patch number.
 set sunny_path="\\sunny\builds\odsxp\%1"
-del /q d:\skpdi_patch\
+REM Clear and recreate stage directory.
+rmdir /s /q d:\skpdi_patch
+md d:\skpdi_patch
 xcopy /e %sunny_path% d:\skpdi_patch\
