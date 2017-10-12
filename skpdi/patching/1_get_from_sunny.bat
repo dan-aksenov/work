2702d17038e824dd4a708e9dd510cb4366f7f31b
dan-aksenov <dan-aksenov@yandex.ru>
2017-10-12 09:47:06 +0300

    patch installer python

--- skpdi/patching/1_get_from_sunny.bat
+++ skpdi/patching/1_get_from_sunny.bat
@@ -1,6 +1,6 @@
 REM create sunny path. %1 is patch number.
 set sunny_path="\\sunny\builds\odsxp\%1"
 REM Clear and recreate stage directory.
-rmdir /s /q d:\skpdi_patch
-md d:\skpdi_patch
+del d:\skpdi_patch\* /S /Q
+
 xcopy /e %sunny_path% d:\skpdi_patch\
