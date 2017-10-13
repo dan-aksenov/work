5a1950e78cbb49c95385c9053734f0707feec13b
dan-aksenov <dan-aksenov@yandex.ru>
2017-10-13 16:27:39 +0300

    hardcoded tablspace stuff commented out.

--- skpdi/db_validate_backup.sh
+++ skpdi/db_validate_backup.sh
@@ -19,22 +19,22 @@ mkdir fdc_log_tab fdc_nsi_tab fdc_ods_big_tab fdc_ods_ind fdc_parameter_ind fdc_
 mkdir fdc_log_ind fdc_nsi_ind fdc_ods_big_ind fdc_ods_geo_ind fdc_ods_tab fdc_secr_tab fdc_parameter_tab
 
 # Use this query on original DB to generate move command for tablespace archives.
-# psql -t ods_prod -c "select 'mv $STAGE_DIR/$CURRENT_BACKUP/'|| oid || '.tar $STAGE_DIR/tablespace/'||spcname from pg_tablespace where spcname not in ('pg_default','pg_global')" | /bin/bash
+psql -t ods_prod -c "select 'mv $STAGE_DIR/$CURRENT_BACKUP/'|| oid || '.tar $STAGE_DIR/tablespace/'||spcname from pg_tablespace where spcname not in ('pg_default','pg_global')" | /bin/bash
 
 # Move tablespace archives to destinagion direcrories.
-mv $STAGE_DIR/$CURRENT_BACKUP/129717.tar $STAGE_DIR/tablespace/fdc_log_ind/
-mv $STAGE_DIR/$CURRENT_BACKUP/129718.tar $STAGE_DIR/tablespace/fdc_log_tab/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129719.tar $STAGE_DIR/tablespace/fdc_nsi_ind/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129720.tar $STAGE_DIR/tablespace/fdc_nsi_tab/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129721.tar $STAGE_DIR/tablespace/fdc_ods_big_ind/
-mv $STAGE_DIR/$CURRENT_BACKUP/129722.tar $STAGE_DIR/tablespace/fdc_ods_big_tab/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129723.tar $STAGE_DIR/tablespace/fdc_ods_geo_ind/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129724.tar $STAGE_DIR/tablespace/fdc_ods_ind/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/129725.tar $STAGE_DIR/tablespace/fdc_ods_tab/ 
-mv $STAGE_DIR/$CURRENT_BACKUP/181169.tar $STAGE_DIR/tablespace/fdc_secr_ind/
-mv $STAGE_DIR/$CURRENT_BACKUP/181170.tar $STAGE_DIR/tablespace/fdc_secr_tab/
-mv $STAGE_DIR/$CURRENT_BACKUP/181171.tar $STAGE_DIR/tablespace/fdc_parameter_ind/
-mv $STAGE_DIR/$CURRENT_BACKUP/181172.tar $STAGE_DIR/tablespace/fdc_parameter_tab/
+#mv $STAGE_DIR/$CURRENT_BACKUP/129717.tar $STAGE_DIR/tablespace/fdc_log_ind/
+#mv $STAGE_DIR/$CURRENT_BACKUP/129718.tar $STAGE_DIR/tablespace/fdc_log_tab/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129719.tar $STAGE_DIR/tablespace/fdc_nsi_ind/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129720.tar $STAGE_DIR/tablespace/fdc_nsi_tab/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129721.tar $STAGE_DIR/tablespace/fdc_ods_big_ind/
+#mv $STAGE_DIR/$CURRENT_BACKUP/129722.tar $STAGE_DIR/tablespace/fdc_ods_big_tab/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129723.tar $STAGE_DIR/tablespace/fdc_ods_geo_ind/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129724.tar $STAGE_DIR/tablespace/fdc_ods_ind/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/129725.tar $STAGE_DIR/tablespace/fdc_ods_tab/ 
+#mv $STAGE_DIR/$CURRENT_BACKUP/181169.tar $STAGE_DIR/tablespace/fdc_secr_ind/
+#mv $STAGE_DIR/$CURRENT_BACKUP/181170.tar $STAGE_DIR/tablespace/fdc_secr_tab/
+#mv $STAGE_DIR/$CURRENT_BACKUP/181171.tar $STAGE_DIR/tablespace/fdc_parameter_ind/
+#mv $STAGE_DIR/$CURRENT_BACKUP/181172.tar $STAGE_DIR/tablespace/fdc_parameter_tab/
 
 # Untar tablespace archives.
 for i in $(ls $STAGE_DIR/tablespace)
@@ -51,19 +51,19 @@ TABLESPACE_LINKS=$STAGE_DIR/$CURRENT_BACKUP/pg_tblspc
 
 #psql -t ods_prod -c "select 'ln -sf $STAGE_DIR/tablespace/'|| spcname || ' $TABLESPACE_LINKS/'|| oid from pg_tablespace where spcname not in ('pg_default','pg_global')"
 
-ln -sf $STAGE_DIR/tablespace/fdc_log_ind $TABLESPACE_LINKS/129717
-ln -sf $STAGE_DIR/tablespace/fdc_log_tab $TABLESPACE_LINKS/129718
-ln -sf $STAGE_DIR/tablespace/fdc_nsi_ind $TABLESPACE_LINKS/129719
-ln -sf $STAGE_DIR/tablespace/fdc_nsi_tab $TABLESPACE_LINKS/129720
-ln -sf $STAGE_DIR/tablespace/fdc_ods_big_ind $TABLESPACE_LINKS/129721
-ln -sf $STAGE_DIR/tablespace/fdc_ods_big_tab $TABLESPACE_LINKS/129722
-ln -sf $STAGE_DIR/tablespace/fdc_ods_geo_ind $TABLESPACE_LINKS/129723
-ln -sf $STAGE_DIR/tablespace/fdc_ods_ind $TABLESPACE_LINKS/129724
-ln -sf $STAGE_DIR/tablespace/fdc_ods_tab $TABLESPACE_LINKS/129725
-ln -sf $STAGE_DIR/tablespace/fdc_secr_ind $TABLESPACE_LINKS/181169
-ln -sf $STAGE_DIR/tablespace/fdc_secr_tab $TABLESPACE_LINKS/181170
-ln -sf $STAGE_DIR/tablespace/fdc_parameter_ind $TABLESPACE_LINKS/181171
-ln -sf $STAGE_DIR/tablespace/fdc_parameter_tab $TABLESPACE_LINKS/181172
+#ln -sf $STAGE_DIR/tablespace/fdc_log_ind $TABLESPACE_LINKS/129717
+#ln -sf $STAGE_DIR/tablespace/fdc_log_tab $TABLESPACE_LINKS/129718
+#ln -sf $STAGE_DIR/tablespace/fdc_nsi_ind $TABLESPACE_LINKS/129719
+#ln -sf $STAGE_DIR/tablespace/fdc_nsi_tab $TABLESPACE_LINKS/129720
+#ln -sf $STAGE_DIR/tablespace/fdc_ods_big_ind $TABLESPACE_LINKS/129721
+#ln -sf $STAGE_DIR/tablespace/fdc_ods_big_tab $TABLESPACE_LINKS/129722
+#ln -sf $STAGE_DIR/tablespace/fdc_ods_geo_ind $TABLESPACE_LINKS/129723
+#ln -sf $STAGE_DIR/tablespace/fdc_ods_ind $TABLESPACE_LINKS/129724
+#ln -sf $STAGE_DIR/tablespace/fdc_ods_tab $TABLESPACE_LINKS/129725
+#ln -sf $STAGE_DIR/tablespace/fdc_secr_ind $TABLESPACE_LINKS/181169
+#ln -sf $STAGE_DIR/tablespace/fdc_secr_tab $TABLESPACE_LINKS/181170
+#ln -sf $STAGE_DIR/tablespace/fdc_parameter_ind $TABLESPACE_LINKS/181171
+#ln -sf $STAGE_DIR/tablespace/fdc_parameter_tab $TABLESPACE_LINKS/181172
 
 # Attempt start.
 pg_ctl -D $STAGE_DIR/$CURRENT_BACKUP start
