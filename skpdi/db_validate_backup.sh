8c7727bc1fb761d5a7b5c99c715d157634b4c5c9
dan-aksenov <dan-aksenov@yandex.ru>
2017-10-13 16:18:22 +0300

    various fixes and improvements

--- skpdi/db_validate_backup.sh
+++ skpdi/db_validate_backup.sh
@@ -1,37 +1,40 @@
-# Move to staging directory.
-
+# Basebackup storage.
+BACKUP_DIR=/mnt/nfs/backup
+# Get latest backup.
+CURRENT_BACKUP=$(ls -t $BACKUP_DIR | head -1)
+# Directory where backup will be tested.
 STAGE_DIR=/usr/local/pgsql/backup_test
-# Directory containing pg_basebackup should be copied here like $STAGE_DIR/db-09-22-2017_09
-STAGE_DATA=$1
 
 cd $STAGE_DIR
+# Copy backup from storage.
+cp $BACKUP_DIR/$CURRENT_BACKUP $STAGE_DIR -r
 
 # Untar main backup archive.
-tar xvf $STAGE_DIR/$STAGE_DATA/base.tar -C $STAGE_DIR/$STAGE_DATA
+tar xvf $STAGE_DIR/$CURRENT_BACKUP/base.tar -C $STAGE_DIR/$CURRENT_BACKUP
 
-# Make directories to create tablespaces.
+# Make directories to hold tablespaces.
 mkdir ./tablespace
 cd ./tablespace
 mkdir fdc_log_tab fdc_nsi_tab fdc_ods_big_tab fdc_ods_ind fdc_parameter_ind fdc_secr_ind
 mkdir fdc_log_ind fdc_nsi_ind fdc_ods_big_ind fdc_ods_geo_ind fdc_ods_tab fdc_secr_tab fdc_parameter_tab
 
 # Use this query on original DB to generate move command for tablespace archives.
-# select 'mv '|| oid || '.tar $STAGE_DIR/tablespace/'||spcname from pg_tablespace;
+# psql -t ods_prod -c "select 'mv $STAGE_DIR/$CURRENT_BACKUP/'|| oid || '.tar $STAGE_DIR/tablespace/'||spcname from pg_tablespace where spcname not in ('pg_default','pg_global')" | /bin/bash
 
 # Move tablespace archives to destinagion direcrories.
-mv $STAGE_DIR/$STAGE_DATA/129717.tar $STAGE_DIR/tablespace/fdc_log_ind/
-mv $STAGE_DIR/$STAGE_DATA/129718.tar $STAGE_DIR/tablespace/fdc_log_tab/ 
-mv $STAGE_DIR/$STAGE_DATA/129719.tar $STAGE_DIR/tablespace/fdc_nsi_ind/ 
-mv $STAGE_DIR/$STAGE_DATA/129720.tar $STAGE_DIR/tablespace/fdc_nsi_tab/ 
-mv $STAGE_DIR/$STAGE_DATA/129721.tar $STAGE_DIR/tablespace/fdc_ods_big_ind/
-mv $STAGE_DIR/$STAGE_DATA/129722.tar $STAGE_DIR/tablespace/fdc_ods_big_tab/ 
-mv $STAGE_DIR/$STAGE_DATA/129723.tar $STAGE_DIR/tablespace/fdc_ods_geo_ind/ 
-mv $STAGE_DIR/$STAGE_DATA/129724.tar $STAGE_DIR/tablespace/fdc_ods_ind/ 
-mv $STAGE_DIR/$STAGE_DATA/129725.tar $STAGE_DIR/tablespace/fdc_ods_tab/ 
-mv $STAGE_DIR/$STAGE_DATA/181169.tar $STAGE_DIR/tablespace/fdc_secr_ind/
-mv $STAGE_DIR/$STAGE_DATA/181170.tar $STAGE_DIR/tablespace/fdc_secr_tab/
-mv $STAGE_DIR/$STAGE_DATA/181171.tar $STAGE_DIR/tablespace/fdc_parameter_ind/
-mv $STAGE_DIR/$STAGE_DATA/181172.tar $STAGE_DIR/tablespace/fdc_parameter_tab/
+mv $STAGE_DIR/$CURRENT_BACKUP/129717.tar $STAGE_DIR/tablespace/fdc_log_ind/
+mv $STAGE_DIR/$CURRENT_BACKUP/129718.tar $STAGE_DIR/tablespace/fdc_log_tab/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129719.tar $STAGE_DIR/tablespace/fdc_nsi_ind/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129720.tar $STAGE_DIR/tablespace/fdc_nsi_tab/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129721.tar $STAGE_DIR/tablespace/fdc_ods_big_ind/
+mv $STAGE_DIR/$CURRENT_BACKUP/129722.tar $STAGE_DIR/tablespace/fdc_ods_big_tab/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129723.tar $STAGE_DIR/tablespace/fdc_ods_geo_ind/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129724.tar $STAGE_DIR/tablespace/fdc_ods_ind/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/129725.tar $STAGE_DIR/tablespace/fdc_ods_tab/ 
+mv $STAGE_DIR/$CURRENT_BACKUP/181169.tar $STAGE_DIR/tablespace/fdc_secr_ind/
+mv $STAGE_DIR/$CURRENT_BACKUP/181170.tar $STAGE_DIR/tablespace/fdc_secr_tab/
+mv $STAGE_DIR/$CURRENT_BACKUP/181171.tar $STAGE_DIR/tablespace/fdc_parameter_ind/
+mv $STAGE_DIR/$CURRENT_BACKUP/181172.tar $STAGE_DIR/tablespace/fdc_parameter_tab/
 
 # Untar tablespace archives.
 for i in $(ls $STAGE_DIR/tablespace)
@@ -40,11 +43,13 @@ find $STAGE_DIR/tablespace/$i -name '*.tar' -exec tar xvf "{}" -C $STAGE_DIR/tab
 done
 
 # Edit postgresql.conf. Restrict listener and change active port.
-sed -i 's/listen_addresses/#listen_addresses/g' $STAGE_DIR/$STAGE_DATA/postgresql.conf
-sed -i 's/port = 5432/port = 54320/g' $STAGE_DIR/$STAGE_DATA/postgresql.conf
+sed -i 's/listen_addresses/#listen_addresses/g' $STAGE_DIR/$CURRENT_BACKUP/postgresql.conf
+sed -i 's/port = 5432/port = 54320/g' $STAGE_DIR/$CURRENT_BACKUP/postgresql.conf
 
 # Relink tablespace links with python script. to be provided.
-TABLESPACE_LINKS=$STAGE_DIR/$STAGE_DATA/pg_tblspc
+TABLESPACE_LINKS=$STAGE_DIR/$CURRENT_BACKUP/pg_tblspc
+
+#psql -t ods_prod -c "select 'ln -sf $STAGE_DIR/tablespace/'|| spcname || ' $TABLESPACE_LINKS/'|| oid from pg_tablespace where spcname not in ('pg_default','pg_global')"
 
 ln -sf $STAGE_DIR/tablespace/fdc_log_ind $TABLESPACE_LINKS/129717
 ln -sf $STAGE_DIR/tablespace/fdc_log_tab $TABLESPACE_LINKS/129718
@@ -61,8 +66,8 @@ ln -sf $STAGE_DIR/tablespace/fdc_parameter_ind $TABLESPACE_LINKS/181171
 ln -sf $STAGE_DIR/tablespace/fdc_parameter_tab $TABLESPACE_LINKS/181172
 
 # Attempt start. This time this'll do.
-pg_ctl -D $STAGE_DIR/$STAGE_DATA start
+pg_ctl -D $STAGE_DIR/$CURRENT_BACKUP start
 
 # Read log to be shure DB is starged.
 DOW=$(date --date=${dateinfile#?_} "+%A"|cut -c -3)
-tail $STAGE_DIR/$STAGE_DATA/pg_log/postgresql-$DOW.log
+tail $STAGE_DIR/$CURRENT_BACKUP/pg_log/postgresql-$DOW.log
