8c2b97a3c9b667924e3b9c04920f767c0eacbec9
dan-aksenov <dan-aksenov@yandex.ru>
2017-12-20 15:17:12 +0300

    stile fixes

--- skpdi/reimport_demo.bat
+++ skpdi/reimport_demo.bat
@@ -3,19 +3,19 @@ set dbname=%1
 set db_dest=-U postgres -h mo-ghkh-dev
 set db_src=-U postgres -h gudhskpdi-db-01
 
-REM create temprorry database
+REM CREATE TEMPRORRY DATABASE
 psql %db_dest% -c "create database %dbname%_tmp with owner ods" postgres
 pause
 
-REM Direct network import database. Exclude event log and parameters data.
+REM DIRECT NETWORK IMPORT DATABASE. EXCLUDE EVENT LOG AND PARAMETERS DATA.
 pg_dump %db_src% -Fp -v --exclude-table-data "event.fdc_app_log_*" --exclude-table-data "parameter.fdc_parameter_md" ods_prod | psql %db_dest% %dbname%_tmp
 pause
 
-REM Reimport parameter_md.
+REM REIMPORT PARAMETER_MD FROM OLD DB.
 pg_dump %db_dest% -Fp -t "parameter.fdc_parameter_md" %dbname% | psql %db_dest% %dbname%_tmp
 pause
 
-REM Rename database.
+REM RENAME DATABASE.
 psql %db_dest% -c "update pg_database set datallowconn = false where datname = '%dbname%'"
 psql %db_dest% -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '%dbname%'"
 psql %db_dest% -c "alter database %dbname% rename to %dbname%_old"
@@ -26,12 +26,12 @@ psql %db_dest% -c "select pg_terminate_backend(pid) from pg_stat_activity where 
 psql %db_dest% -c "alter database %dbname%_tmp rename to %dbname%"
 pause
 
-REM Drop old database
+REM DROP OLD DATABASE.
 psql %db_dest% -c "update pg_database set datallowconn = false where datname = '%dbname%_old"
 psql %db_dest% -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '%dbname%_old'"
 psql %db_dest% -c "drop database %dbname%_old"
 pause
 
-REM Allow connections.
+REM ALLOW CONNECTIONS.
 psql %db_dest% -c "update pg_database set datallowconn = true where datname = '%dbname%'"
 pause
