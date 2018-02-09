b6e289709c3d7a4b5b52ed24c9eb36d83623c67f
dan-aksenov <dan-aksenov@yandex.ru>
2018-02-09 18:26:15 +0300

    endpoint log quiery added

---
+++ pts/quieries/smev_check_endpoint_log.sql
@@ -0,0 +1,1 @@
+select t.status, t.send_date, t.original_message_id, t.endpoint_code from EXCHANGE_ENDPOINT_LOG t where endpoint_code in ('TEST_SMEV_SUPPLIER','TEST_SMEV_CONSUMER') order by t.send_date limit 10;
