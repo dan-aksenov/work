827ea1ed7ab328575d46a17644628da1491e0b0f
dan-aksenov <dan-aksenov@yandex.ru>
2018-02-09 13:36:38 +0300

    fixes

--- pts/smev_get_xml.py
+++ pts/smev_get_xml.py
@@ -42,7 +42,7 @@ def main():
     '''
     Поиск и отбор xml
     '''
-    xmls = postgres_exec ( "select t.status, t.operation, t.send_date, t.error, t.message_id, t.original_message_id, t.content,t.sent_response from EXCHANGE_ENDPOINT_LOG t where endpoint_code in ('TEST_SMEV_SUPPLIER','TEST_SMEV_CONSUMER') and status in ('ACK_OK','RECEIVED','SENT','RESPONSE_RECEIVED') order by t.send_date" )
+    xmls = postgres_exec ( "select t.status, t.operation, t.send_date, t.error, t.message_id, t.original_message_id, t.content,t.sent_response,t.id from EXCHANGE_ENDPOINT_LOG t where endpoint_code in ('TEST_SMEV_SUPPLIER','TEST_SMEV_CONSUMER') and status in ('ACK_OK','RECEIVED','SENT','RESPONSE_RECEIVED') order by t.send_date" )
     
     '''
     Каждый найденный xml запивать в файл вида original_message_id_status.xml
@@ -54,7 +54,7 @@ def main():
         if not os.path.exists( dir ):
             os.makedirs( dir )
         # Задать имя файла
-        out_file = ( xmls[i][5] + "_" + xmls[i][0] +  ".xml" )
+        out_file = ( xmls[i][5] + "_" + xmls[i][0] + "_" + xmls[i][8] +  ".xml" )
         f = open( dir + "\\" + out_file, 'w' )
         # Проверка sent_response:
         # если ненулевой то записать в файл и его тоже
