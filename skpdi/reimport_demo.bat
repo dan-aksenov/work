659b950c46daf077f473480979b0e1ce71c517a9
dan-aksenov <dan-aksenov@yandex.ru>
2018-02-16 21:17:54 +0300

    port changed

--- skpdi/reimport_demo.bat
+++ skpdi/reimport_demo.bat
@@ -1,6 +1,6 @@
 set dbname=%1
 
-set db_dest=-U postgres -p 54320 -h mo-ghkh-dev
+set db_dest=-U postgres -p 5432 -h mo-ghkh-dev
 set db_src=-U postgres -h gudhskpdi-db-01
 
 REM CREATE TEMPRORRY DATABASE
