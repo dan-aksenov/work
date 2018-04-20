e6f93c22037edfac07dc6d1aa19e7fa76795b72a
dan-aksenov <dan-aksenov@yandex.ru>
2018-04-20 16:51:51 +0300

    fix

--- pts/patch.bat
+++ pts/patch.bat
@@ -10,9 +10,9 @@ set plink_cmd=plink -i %ssh_key% %usr_nix%@%dst_host%
 rem Rename war files to desired names.
 cd /d %stage_dir%
 ren pts-integration-*.war integration.war
-ren pts-public-*.war portal.war;
+ren pts-public-*.war mobile.war;
 ren pts-restricted-*.war pts.war
-ren pts-portal*.war portal2.war
+ren pts-portal*.war portal.war
 ren pts-jointstorage*.war jointstorage.war
 
 rem Stop tomcat.
@@ -20,15 +20,15 @@ rem Stop tomcat.
 
 rem Remove old app files and dirs
 %plink_cmd% "sudo rm %app_name%/webapps/integration.war -f"
-%plink_cmd% "sudo rm %app_name%/webapps/portal.war -f"
+%plink_cmd% "sudo rm %app_name%/webapps/mobile.war -f"
 %plink_cmd% "sudo rm %app_name%/webapps/pts.war -f"
-%plink_cmd% "sudo rm %app_name%/webapps/portal2.war"
+%plink_cmd% "sudo rm %app_name%/webapps/portal.war"
 %plink_cmd% "sudo rm %app_name%/webapps/jointstorage.war"
 
 %plink_cmd% "sudo rm %app_name%/webapps/integration -rf"
-%plink_cmd% "sudo rm %app_name%/webapps/portal -rf"
+%plink_cmd% "sudo rm %app_name%/webapps/mobile -rf"
 %plink_cmd% "sudo rm %app_name%/webapps/pts -rf"
-%plink_cmd% "sudo rm %app_name%/webapps/portal2 -rf"
+%plink_cmd% "sudo rm %app_name%/webapps/portal -rf"
 %plink_cmd% "sudo rm %app_name%/webapps/jointstorage -rf"
 
 rem Copy files to nix machine.
@@ -40,12 +40,14 @@ pscp -i %ssh_key% *.war %usr_nix%@%dst_host%:/tmp/webapps
 rem Check files md5.
 md5sum integration.war
 %plink_cmd% "sudo md5sum %app_name%/webapps/integration.war"
-md5sum portal.war
-%plink_cmd% "sudo md5sum %app_name%/webapps/portal.war"
+md5sum mobile.war
+%plink_cmd% "sudo md5sum %app_name%/webapps/mobile.war"
 md5sum pts.war
 %plink_cmd% "sudo md5sum %app_name%/webapps/pts.war"
-md5sum portal2.war
-%plink_cmd% "sudo md5sum %app_name%/webapps/portal2.war"
+md5sum portal.war
+%plink_cmd% "sudo md5sum %app_name%/webapps/portal.war"
+md5sum jointstorage.war
+%plink_cmd% "sudo md5sum %app_name%/webapps/jointstorage.war"
 
 rem Start tomcat.
 %plink_cmd% "sudo systemctl start tomcat%2"
