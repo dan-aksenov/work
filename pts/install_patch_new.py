d3c692ab8be7a337219498f237c3cf14aaa83f2b
dan-aksenov <dan-aksenov@yandex.ru>
2018-04-23 18:22:19 +0300

    new patch installer added.

---
+++ pts/install_patch_new.py
@@ -0,0 +1,6 @@
+# copy war files to asnible host
+
+# check database version and apply database patch
+
+# run serial task to update apps one by one from ansible host.
+# will restart of servers be serial too&
