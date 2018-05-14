7e724005a85e7814b9ac22901f982b56ae95580d
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-14 11:21:23 +0300

    stile improvements

--- patches/skpdi-prod.py
+++ patches/skpdi-prod.py
@@ -19,15 +19,19 @@ for opt, arg in opts:
         print "-n for patch number"
         sys.exit()
 
-#Variables
+# Variables
+# host to run ansible commands from
 jump_host = "oemcc.fors.ru"
+ansible_inventory = '~/ansible-hosts/skpdi-prod'
 # application hosts as writen in ansible invenrory
 application_hosts = ['gudhskpdi-app-02', 'gudhskpdi-app-01']
-# // so windows can also read in correctly
+# // so windows can also read it correctly, same as linux
 sunny_path= '//sunny/builds/odsxp/'
+# tomcat application location
 application_path = '/u01/apache-tomcat-8.5.8/webapps/'
+# sysinit or systemd service name to stop/start server
 tomcat_name = 'tomcat'
-ansible_inventory = '~/ansible-hosts/skpdi-prod'
+# war files mappings
 wars = [
     [ 'skpdi-' + patch_num + '.war', 'skpdi' ],['ext-' + patch_num + '.war', 'ext']
     ]
@@ -35,11 +39,13 @@ wars = [
 db_host = 'gudhskpdi-db-01'
 db_name = 'ods_prod'
 db_user = 'ods'
+# databaes table to look for current db_version
 patch_table = 'parameter.fdc_patches_log'
+# temporary directory to hold database patches.
 stage_dir = 'd:/tmp/skpdi_patch'
 
-c = PatchDatabase(patch_num, sunny_path, application_hosts, ansible_inventory, db_host, db_name, stage_dir, db_user, patch_table)
-c.patchdb()
+d = PatchDatabase(patch_num, sunny_path, application_hosts, ansible_inventory, db_host, db_name, stage_dir, db_user, patch_table)
+d.patchdb()
 
 a = ApplicationUpdate( jump_host, patch_num, sunny_path, application_hosts, application_path, tomcat_name, ansible_inventory, wars )
 a.application_update()
