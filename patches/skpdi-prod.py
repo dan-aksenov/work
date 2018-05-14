a9b055736cfc69c3055ac8391ddef438e1c4dd30
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-14 10:49:39 +0300

    skpdi prod modified

--- patches/skpdi-prod.py
+++ patches/skpdi-prod.py
@@ -1,4 +1,5 @@
 from application_update import ApplicationUpdate
+from patch_database import PatchDatabase
 import utils
 
 from getopt import getopt
@@ -20,10 +21,10 @@ for opt, arg in opts:
 
 #Variables
 jump_host = "oemcc.fors.ru"
-
-sunny_path= '/sunny/builds/odsxp/'
 # application hosts as writen in ansible invenrory
-application_hosts = [ 'gudhskpdi-app-01', 'gudhskpdi-app-02' ]
+application_hosts = ['gudhskpdi-app-02', 'gudhskpdi-app-01']
+# // so windows can also read in correctly
+sunny_path= '//sunny/builds/odsxp/'
 application_path = '/u01/apache-tomcat-8.5.8/webapps/'
 tomcat_name = 'tomcat'
 ansible_inventory = '~/ansible-hosts/skpdi-prod'
@@ -31,6 +32,14 @@ wars = [
     [ 'skpdi-' + patch_num + '.war', 'skpdi' ],['ext-' + patch_num + '.war', 'ext']
     ]
 
-a = ApplicationUpdate( jump_host, patch_num, sunny_path, application_hosts, application_path, tomcat_name, ansible_inventory, wars )
+db_host = 'gudhskpdi-db-01'
+db_name = 'ods_prod'
+db_user = 'ods'
+patch_table = 'parameter.fdc_patches_log'
+stage_dir = 'd:/tmp/skpdi_patch'
 
+c = PatchDatabase(patch_num, sunny_path, application_hosts, ansible_inventory, db_host, db_name, stage_dir, db_user, patch_table)
+c.patchdb()
+
+a = ApplicationUpdate( jump_host, patch_num, sunny_path, application_hosts, application_path, tomcat_name, ansible_inventory, wars )
 a.application_update()
