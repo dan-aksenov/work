a9ca9a00fcf7be35bb60e1f3f293fc83b45981f2
dan-aksenov <dan-aksenov@yandex.ru>
2018-05-18 15:29:12 +0300

    pycodestile

--- patches/skpdi-prod.py
+++ patches/skpdi-prod.py
@@ -3,17 +3,18 @@ from patch_database import PatchDatabase
 import utils
 
 from getopt import getopt
-import sys, os
+import sys
+import os
 
 # Get patch number and target environment from parameters n and t
-try:    
-    opts, args = getopt( sys.argv[1:], 'n:' )
-except:
+try:
+    opts, args = getopt(sys.argv[1:], 'n:')
+except StandardError:
     print "-n for patch number"
     sys.exit()
 
 for opt, arg in opts:
-    if opt in ( '-n' ):
+    if opt in ('-n'):
         patch_num = arg
     else:
         print "-n for patch number"
@@ -26,14 +27,15 @@ ansible_inventory = '~/ansible-hosts/skpdi-prod'
 # application hosts as writen in ansible invenrory
 application_hosts = ['gudhskpdi-app-02', 'gudhskpdi-app-01']
 # // so windows can also read it correctly, same as linux
-sunny_path= '//sunny/builds/odsxp/'
+sunny_path = '//sunny/builds/odsxp/'
 # tomcat application location
 application_path = '/u01/apache-tomcat-8.5.8/webapps/'
 # sysinit or systemd service name to stop/start server
 tomcat_name = 'tomcat'
 # war files mappings
 wars = [
-    [ 'skpdi-' + patch_num + '.war', 'skpdi' ],['ext-' + patch_num + '.war', 'ext']
+    ['skpdi-' + patch_num + '.war', 'skpdi'],
+    ['ext-' + patch_num + '.war', 'ext']
     ]
 
 db_host = 'gudhskpdi-db-01'
@@ -44,8 +46,29 @@ patch_table = 'parameter.fdc_patches_log'
 # temporary directory to hold database patches.
 stage_dir = 'd:/tmp/skpdi_patch'
 
-d = PatchDatabase(patch_num, sunny_path, application_hosts, ansible_inventory, db_host, db_name, stage_dir, db_user, patch_table)
+d = PatchDatabase(
+    patch_num,
+    sunny_path,
+    application_hosts,
+    ansible_inventory,
+    db_host,
+    db_name,
+    stage_dir,
+    db_user,
+    patch_table
+   )
+
 d.patchdb()
 
-a = ApplicationUpdate( jump_host, patch_num, sunny_path, application_hosts, application_path, tomcat_name, ansible_inventory, wars )
+a = ApplicationUpdate(
+    jump_host,
+    patch_num,
+    sunny_path,
+    application_hosts,
+    application_path,
+    tomcat_name,
+    ansible_inventory,
+    wars
+   )
+
 a.application_update()
