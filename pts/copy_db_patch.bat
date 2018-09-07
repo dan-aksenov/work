7caa87a402a02bb18405717e2c22a75eab452e12
dan-aksenov <dan-aksenov@yandex.ru>
2018-09-07 16:08:20 +0300

    comments added

--- pts/copy_db_patch.bat
+++ pts/copy_db_patch.bat
@@ -1,5 +1,5 @@
 REM copy patch executor to database patch subfolders.
-REM parameter %1 to copy demo of prod(skpdi) patch
+REM parameter %1 to copy master or branch patch
 for /D %%a in ("d:\pts_patch\*") do xcopy /y /d .\%1\db-%1.bat "%%a\patches\"
 for /D %%a in ("d:\pts_patch\*") do xcopy /y /d .\%1\db-trans-%1.bat "%%a\jointstorage_patches\"
 
