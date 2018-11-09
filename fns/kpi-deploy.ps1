5bcf47f616b912d72c3b6ce57dda2a89e0336d15
dan-aksenov <dan-aksenov@yandex.ru>
2018-11-09 09:59:40 +0300

    added index script

--- fns/kpi-deploy.ps1
+++ fns/kpi-deploy.ps1
@@ -13,61 +13,61 @@ $Stamp = $(get-date -f yyyyMMdd_hh_mm_ss)
 try {
 New-PSDrive –Name "K" –PSProvider FileSystem –Root "\\$DeployHost\D$" -Credential $Cred
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 try {
 Copy-Item -Path kpi-ta\Build\Debug\Build -Destination K:\Builds\Build$Stamp –Recurse
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 #copy deploy scripts
 try {
 Copy-Item -Path kpi-ta\Source\TRC\Source\Ais3.TRC.Resources\Scripts\trc-deploy.ps1 -Destination "K:\Builds\" -Force
 Copy-Item -Path kpi-ta\Source\TRC\Source\Ais3.TRC.Resources\Scripts\trc-graceful-teardown-local.ps1 -Destination "K:\Builds\" -Force
 Copy-Item -Path kpi-ta\Source\TRC\Source\Ais3.TRC.Resources\Scripts\trc-azman.psm1 -Destination "K:\Builds\" -Force
+Copy-Item -Path kpi-ta\Source\TRC\Source\Ais3.TRC.Resources\Scripts\trc-index.ps1 -Destination "K:\Builds\" -Force
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 #run deployment scripts
 
 try {
-Invoke-command –computername $DeployHost -credential $Cred –scriptblock { D:\Builds\trc-graceful-teardown-local.ps1 }
+Invoke-command –computername 192.168.118.7 -credential $Cred –scriptblock { D:\Builds\trc-graceful-teardown-local.ps1 }
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 try {
 Remove-Item -Recurse -Force K:\AIS3
-#Copy-Item -Path kpi-ta\Build\Debug\Build -Destination K:\AIS3 –Recurse -Force
-Invoke-command –computername $DeployHost -credential $Cred –scriptblock { Copy-Item -Path K:\Builds\Build$Stamp -Destination K:\AIS3 –Recurse -Force }
+Copy-Item -Path kpi-ta\Build\Debug\Build -Destination K:\AIS3 –Recurse -Force
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 try {
-Invoke-command –computername $DeployHost -credential $Cred -argumentlist $Stamp –scriptblock { 
+Invoke-command –computername 192.168.118.7 -credential $Cred -argumentlist $Stamp –scriptblock { 
 Set-Location -Path "D:\Builds"
 D:\Builds\trc-deploy.ps1 d:\AIS3 }
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 #compress build
 try {
-Invoke-command –computername $DeployHost -credential $Cred -argumentlist $Stamp –scriptblock {
+Invoke-command –computername 192.168.118.7 -credential $Cred -argumentlist $Stamp –scriptblock {
 $src = "d:\Builds\Build$args"
 $dst = "d:\Builds\Build$args.zip"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory($src, $dst)
 }
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 #delete folder
 try {
 Remove-Item K:\Builds\Build$Stamp -Force -Recurse
 }
-Catch { BREAK }
+Catch {  BREAK  }
 
 # Remove net drive
 try {
 Remove-PSDrive -Name "K"
 }
-Catch { BREAK }
+Catch {  BREAK  }
