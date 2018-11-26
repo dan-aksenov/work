cbb956b4ee16bc29acae5ce1f8cb71f2a7f1785b
dan-aksenov <dan-aksenov@yandex.ru>
2018-11-26 17:23:04 +0300

    class reworked

--- skpdi/locust/locustfile.py
+++ skpdi/locust/locustfile.py
@@ -1,25 +1,27 @@
-from locust import HttpLocust, TaskSet
-
-def login(l):
-    l.client.post("/login", {"username":"Admin", "password":"admin321"})
-
-def logout(l):
-    l.client.post("/logout")
-
-def index(l):
-    l.client.get("/")
-
-def MobileMissionFactRegistryView(l):
-    l.client.get("/registry?code=additionRegistryForm&bk=ODH/Base/MobileMissionFactRegistryView")
+from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
-    tasks = {index: 2, MobileMissionFactRegistryView: 1}
-
     def on_start(self):
-        login(self)
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.login()
 
     def on_stop(self):
-        logout(self)
+        """ on_stop is called when the TaskSet is stopping """
+        self.logout()
+
+    def login(self):
+        self.client.post("/login", {"username":"admin", "password":"admin"})
+
+    def logout(self):
+        self.client.post("/logout", {"username":"admin", "password":"admin"})
+
+    @task(2)
+    def index(self):
+        self.client.get("/")
+
+    @task(1)
+    def profile(self):
+        self.client.get("/registry?code=additionRegistryForm&bk=ODH/Base/MobileMissionFactRegistryView")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
