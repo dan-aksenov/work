4fb5181c649189f35e8300ad16ef97858263d6ef
dan-aksenov <dan-aksenov@yandex.ru>
2019-01-11 17:09:53 +0300

    more fixes and finalizetions

--- skpdi/get_dump.sh
+++ skpdi/get_dump.sh
@@ -1,9 +1,5 @@
-# export dbname=$1
-
 src_host=gudhskpdi-db-01
 
-echo ###################################################################################
 echo Dump and download ODS_PROD database.
-time ssh $src_host sudo -u postgres pg_dump ods_prod -Fc -Z5 -v --exclude-table-data "event.fdc_app_log_*" --exclude-table-data "parameter.fdc_parameter_md" -f /backup/dump/prod.dmp
+time ssh $src_host sudo -u postgres pg_dump ods_prod -Fc -Z5 --exclude-table-data "event.fdc_app_log_*" --exclude-table-data "parameter.fdc_parameter_md" -f /backup/dump/prod.dmp
 time scp $src_host:/backup/dump/prod.dmp /tmp/
-echo ###################################################################################
--- skpdi/reimport.sh
+++ skpdi/reimport.sh
@@ -5,11 +5,12 @@ export db_dest="-U postgres -p 5432 -h $dst_host"
 
 echo Create temporary database ${dbname}_tmp
 psql $db_dest -c "create database ${dbname}_tmp with owner ods" postgres
-psql $db_dest -c "select datname, (pg_stat_file('base/'||oid||'/PG_VERSION')).modification AS datcreated from pg_database where datname = '${dbname}_tmp'"
+psql $db_dest -c "select datname, pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname)) AS Size, (pg_stat_file('base/'||oid||'/PG_VERSION')).modification AS datcreated from pg_database where datname = '${dbname}_tmp'"
 
 read -p "Press [Enter] key import source ODS_PROD database to ${dbname}_tmp. Exclude event log and parameters data."
 echo Import source ODS_PROD database to ${dbname}_tmp. Exclude event log and parameters data.
 time pg_restore $db_dest --jobs=4 -d ${dbname}_tmp /tmp/prod.dmp 2>/tmp/${dbname}_import.log
+psql $db_dest -c "select datname, pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname)) AS Size, (pg_stat_file('base/'||oid||'/PG_VERSION')).modification AS datcreated from pg_database where datname = '${dbname}_tmp'"
 
 read -p "Press [Enter] key to reimport parameter_md from old db and reset admin password"
 pg_dump $db_dest -Fp --data-only -t "parameter.fdc_parameter_md" $dbname | psql $db_dest ${dbname}_tmp
@@ -24,11 +25,8 @@ read -p "Press [Enter] key to rename database ${dbname}_tmp to $dbname"
 psql $db_dest -c "update pg_database set datallowconn = false where datname = '${dbname}_tmp'"
 psql $db_dest -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '${dbname}_tmp'"
 psql $db_dest -c "alter database ${dbname}_tmp rename to $dbname"
-read -p "Press [Enter] key to proceed..."
-
-echo Allow connections to $dbname
 psql $db_dest -c "update pg_database set datallowconn = true where datname = '$dbname'"
-psql $db_dest -c "select datallowconn from pg_database where datname = '$dbname'"
+psql $db_dest -c "select datname, pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname)) AS Size, (pg_stat_file('base/'||oid||'/PG_VERSION')).modification AS datcreated from pg_database where datname = '${dbname}'"
 
 read -p "Press [Enter] to drop old database ${dbname}_old or CTRL+C to exit batch"
 psql $db_dest -c "update pg_database set datallowconn = false where datname = '${dbname}_old'"
