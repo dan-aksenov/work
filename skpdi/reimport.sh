430e75ea7853c635fef689f707829363680d43d4
dan-aksenov <dan-aksenov@yandex.ru>
2019-01-11 15:21:19 +0300

    compression added, stile fixed

--- skpdi/reimport.sh
+++ skpdi/reimport.sh
@@ -1,41 +1,36 @@
 # Variables to be passed from uper script.
 export dbname=$1
-# export db_dest=-U postgres -p 5432 -h skpdi-test-db
-# export db_src=-U postgres -h gudhskpdi-db-01
+export dst_host=$2
+export db_dest="-U postgres -p 5432 -h $dst_host"
 
-echo ###################################################################################
 echo Create temporary database ${dbname}_tmp
 psql $db_dest -c "create database ${dbname}_tmp with owner ods" postgres
 psql $db_dest -c "select datname, (pg_stat_file('base/'||oid||'/PG_VERSION')).modification AS datcreated from pg_database where datname = '${dbname}_tmp'"
-read -p "Press [Enter] key to proceed..."
-echo ###################################################################################
+
+read -p "Press [Enter] key import source ODS_PROD database to ${dbname}_tmp. Exclude event log and parameters data."
 echo Import source ODS_PROD database to ${dbname}_tmp. Exclude event log and parameters data.
-psql $db_dest -f /tmp/$dbname.sql ${dbname}_tmp 2>/tmp/${dbname}_import.log
-read -p "Press [Enter] key to proceed..."
-echo ###################################################################################
-echo Reimport parameter_md from old db
-pg_dump $db_dest -Fp -t "parameter.fdc_parameter_md" $dbname | psql $db_dest ${dbname}_tmp
-read -p "Press [Enter] key to proceed..."
-echo ###################################################################################
-echo Rename database $dbname to ${dbname}_old
+time pg_restore $db_dest --jobs=4 -d ${dbname}_tmp /tmp/prod.dmp 2>/tmp/${dbname}_import.log
+
+read -p "Press [Enter] key to reimport parameter_md from old db and reset admin password"
+pg_dump $db_dest -Fp --data-only -t "parameter.fdc_parameter_md" $dbname | psql $db_dest ${dbname}_tmp
+psql $db_dest -c "UPDATE secr.fdc_user_md SET passwd = '$2a$10$GwiQ4p7I6Si/Cr5LJN60duTrpjGVsTFn47ELx1kbRW7RPUD6ucuqy' WHERE sysname = 'ADMIN'" ${dbname}_tmp
+
+read -p "Press [Enter] key to rename database $dbname to ${dbname}_old"
 psql $db_dest -c "update pg_database set datallowconn = false where datname = '$dbname'"
 psql $db_dest -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '$dbname'"
 psql $db_dest -c "alter database $dbname rename to ${dbname}_old"
-read -p "Press [Enter] key to proceed..."
-echo ###################################################################################
-echo Rename database ${dbname}_tmp to $dbname
+
+read -p "Press [Enter] key to rename database ${dbname}_tmp to $dbname"
 psql $db_dest -c "update pg_database set datallowconn = false where datname = '${dbname}_tmp'"
 psql $db_dest -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '${dbname}_tmp'"
 psql $db_dest -c "alter database ${dbname}_tmp rename to $dbname"
 read -p "Press [Enter] key to proceed..."
-echo ###################################################################################
+
 echo Allow connections to $dbname
 psql $db_dest -c "update pg_database set datallowconn = true where datname = '$dbname'"
 psql $db_dest -c "select datallowconn from pg_database where datname = '$dbname'"
 
-echo ###################################################################################
-echo Drop old database ${dbname}_old? Or CTRL+C to exit batch
-read -p "Press [Enter] key to proceed..."
+read -p "Press [Enter] to drop old database ${dbname}_old or CTRL+C to exit batch"
 psql $db_dest -c "update pg_database set datallowconn = false where datname = '${dbname}_old'"
 psql $db_dest -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '${dbname}_old'"
 psql $db_dest -c "drop database ${dbname}_old"
