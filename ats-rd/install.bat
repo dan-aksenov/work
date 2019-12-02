e24634fe41b858d456abd0e9ead9d19a27000e39
dbax <dan-aksenov@yandex.ru>
2019-12-02 13:43:35 +0300

    ats-rd patcher added.
    + many more

---
+++ ats-rd/install.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+chcp 65001
+
+rem Контур разработки
+set PGHOST=ats-rd.fors.ru
+rem set PGPORT=5432
+
+set PGUSER=postgres
+set PGDATABASE=ats_test
+set PGPASSWORD=postgres
+
+echo Connecting to %PGDATABASE%@%PGHOST%:%PGPORT%
+
+pause
+
+psql <install.sql >install.log 2>&1
---
+++ ats-rd/test.sh
@@ -0,0 +1,30 @@
+if [ "$#" -ne 1 ]; then
+    echo "$1 for patch number" 
+    exit 1
+fi
+
+ver=$1
+
+ssh ansible@ats-rd sudo pkill java
+ssh ansible@ats-rd sudo rm /u01/apache-tomcat-8.5.8/webapps/ats-rd* -rf
+
+sudo mount -t drvfs '\\sunny\builds\ats\' /mnt/sunny
+rsync -avz /mnt/sunny/$ver /tmp/stage
+cd /tmp/stage/$ver/patch
+psql -h ats-rd -p 5432 -U postgres ats-test<install.sql &/tmp/install_$ver.log
+
+tail /tmp/install_$ver.log
+read -p "Press [Enter] key to proceed..."
+
+ssh ansible@ats-rd mkdir /tmp/stage
+scp /tmp/stage/$ver/ats-rd-server-$ver.war ansible@ats-rd:/tmp/stage
+ssh ansible@ats-rd sudo cp /tmp/stage/ats-rd-server-$ver.war /u01/apache-tomcat-8.5.8/webapps/ats-rd.war
+
+md5sum /mnt/sunny/$ver/ats-rd-server-$ver.war
+ssh ansible@ats-rd sudo md5sum /u01/apache-tomcat-8.5.8/webapps/ats-rd.war
+read -p "Press [Enter] key to proceed..."
+ssh ansible@ats-rd sudo -u tomcat /u01/apache-tomcat-8.5.8/bin/startup.sh
+
+cd ~
+rm -rf /tmp/stage
+sudo umount /mnt/sunny
